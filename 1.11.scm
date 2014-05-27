e66049059b487ce004de59e8b127077f5a35852a
Stephen Olsen <steve.olsen@gmail.com>
2014-05-27 19:50:23 -0400

    don't need n twice

--- 1.11.scm
+++ 1.11.scm
@@ -1,4 +1,9 @@
 ;; Recursive
+;;f 1 = 1
+;;f 2 = 2
+;;f 3 = 3
+;;f n = f(n-1) + 2f(n-2) + 3f(n-3)
+
 (define (rec-f n)
   (cond
    ((<= n 3) n)
@@ -8,13 +13,13 @@
 
 ;; Iterative
 (define (iter-f n)
-  (define (iter back1 back2 back3 at n)
+  (define (iter back1 back2 back3 at)
     (define current (+ back1
                        (* 2 back2)
                        (* 3 back3)))
     (cond
      ((= at n) current)
-     (else (iter current back1 back2 (+ at 1) n))))
+     (else (iter current back1 back2 (+ at 1)))))
   (cond
    ((<= n 3) n)
-   (else (iter 3 2 1 4 n))))
+   (else (iter 3 2 1 4))))
